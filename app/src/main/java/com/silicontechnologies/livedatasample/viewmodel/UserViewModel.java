cfea88efd95958f573c0ba429827b9e04983f1a6
“Raja <rajajawahar77@gmail.com>
2018-01-23 19:33:18 +0530

    RxJava and RxAndroid added

--- app/src/main/java/com/silicontechnologies/livedatasample/viewmodel/UserViewModel.java
+++ app/src/main/java/com/silicontechnologies/livedatasample/viewmodel/UserViewModel.java
@@ -1,6 +1,8 @@
 package com.silicontechnologies.livedatasample.viewmodel;
 
 import android.arch.lifecycle.ViewModel;
+import com.silicontechnologies.livedatasample.app.AppController;
+import com.silicontechnologies.livedatasample.db.AppDatabase;
 import com.silicontechnologies.livedatasample.entities.User;
 
 /**
@@ -11,6 +13,11 @@ public class UserViewModel extends ViewModel {
 
   private String userId;
   private User user;
+  private AppDatabase appDatabase;
+
+  public UserViewModel() {
+    appDatabase = AppController.getAppDatabase();
+  }
 
   public void init(String userId) {
     this.userId = userId;
@@ -19,6 +26,4 @@ public class UserViewModel extends ViewModel {
   public User getUser() {
     return user;
   }
-
-
 }
